62ac838a0e042ba8a452ad707fb4982908fe0b95
TomEmment <61800951+TomEmment@users.noreply.github.com>
2020-03-04 18:27:39 +0000

    Initial commit

---
+++ Test/src/test/Main.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+
+
+public class Main{
+
+    public static void main(String[] args) {
+      LogInScreen Instance = new LogInScreen();
+      Instance.setVisible(true);
+
+    
+    }
+
+}
---
+++ Test/src/test/Plot_Data.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import java.awt.Color;  
+import javax.swing.JFrame;  
+import javax.swing.SwingUtilities;  
+import javax.swing.WindowConstants;  
+import org.jfree.chart.ChartFactory;  
+import org.jfree.chart.ChartPanel;  
+import org.jfree.chart.JFreeChart;  
+import org.jfree.chart.plot.XYPlot;  
+import org.jfree.data.xy.XYDataset;  
+import org.jfree.data.xy.XYSeries;  
+import org.jfree.data.xy.XYSeriesCollection;  
+  
+public class Plot_Data extends JFrame {  
+  private static final long serialVersionUID = 6294689542092367723L;  
+  public Plot_Data(String title, String variable, int Data[][], int Instance) {  
+    super(title);  
+  
+    // Create dataset  
+    XYDataset dataset = createDataset(variable, Data, Instance);  
+  
+    // Create chart  
+    JFreeChart chart = ChartFactory.createScatterPlot(  
+        variable+" Vs time",   
+        "Time", variable, dataset); 
+    // Create Panel  
+    ChartPanel panel = new ChartPanel(chart);  
+    setContentPane(panel);  
+  }  
+  
+  private XYDataset createDataset(String variable, int Data[][], int Instance) {  
+    XYSeriesCollection dataset = new XYSeriesCollection();  
+    XYSeries series1 = new XYSeries(variable);  
+    int x = 1;
+    while(Data[Instance][x]!=0){
+        series1.add(Data[0][x], Data[Instance][x]); 
+        x=x+1;
+    }
+
+    dataset.addSeries(series1);  
+    return dataset;  
+  }  
+  public static void main(String[] args) {  
+      
+  }  
+}  
