68dc490cf7e24a8074729ac5007349cae1a8dddf
HarryEvs21 <56265991+HarryEvs21@users.noreply.github.com>
2020-04-07 16:52:49 +0100

    merging mine

---
+++ SystemSoftware/SystemSoftware/src/farmerclientserver/WeatherStation.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmerclientserver;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+/**
+ *
+ * @author Harry
+ */
+
+public class WeatherStation {
+
+    static Socket s;
+    static DataInputStream din;
+    static DataOutputStream dout;
+    
+    
+    WeatherStation(List<String> Files){
+
+    }
+    
+    //Define all of the methods
+    //Set all of the methods
+    //Updating each variable
+    
+
+ 
+
+    public void main(String args[])throws Exception
+    {
+        String Message ="";
+          try (ServerSocket ss = new ServerSocket(1202)) {
+            Socket s = ss.accept();
+            while (!Message.equals("exit")){
+                Message = din.readUTF();
+            }
+            //Read Message
+            //First Letter = File, Second Variuable X, Third Variable Y
+            System.out.println("Test3");
+            String NewMessage ="";
+            NewMessage = getDataX() +getDataY(); 
+            dout.writeUTF(NewMessage);
+            
+            
+          }
+          
+          catch(Exception e){
+              
+          }
+              
+         
+          
+    }
+  public String getDataX(){
+         
+       String VariableX = "12,23,5,43,7,8";
+       return VariableX;
+    }   
+   public String getDataY(){
+         
+       String VariableY = "13,45,76,56,65";
+       return VariableY;
+    }   
+}
--- SystemSoftware/src/farmerclientserver/FarmerClientServer.java
+++ SystemSoftware/src/farmerclientserver/FarmerClientServer.java
@@ -9,18 +9,109 @@ package farmerclientserver;
  *
  * @author Thoma
  */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
-
+import java.util.Random; 
 
 
 public class FarmerClientServer {
 
     public static void main(String[] args) {
+      /*
+      InitializeFiles("Station1.txt",12);
+      InitializeFiles("Station2.txt",12);
+      InitializeFiles("Station3.txt",12);
+      InitializeFiles("Station4.txt",12);
+         */
       LogInScreen Instance = new LogInScreen();
       Instance.setVisible(true);
-
+            
     
     }
+    public static void InitializeFiles(String FileName, int Length)
+    {
+        
+        int Start =8;
+        int Start1 = 37;
+        int Start2 = 20;
+        int Start3 = 60;
+        int Randomizer;
+        int temp;
+        char Station  = FileName.charAt(7);
+        String StationRef = Character.toString(Station);
+        String Data0 = ":0:";
+        String Data1 = ":1:";
+        String Data2 = ":2:";
+        String Data3 = ":3:";
+        Random rand = new Random();
+        
 
+        while (Length>=0)
+        {
+        Randomizer = rand.nextInt(10);
+        temp = Start + Randomizer;
+        if (Length!=0)
+        {
+        Data0 = Data0 + Integer.toString(temp)+",";
+        }else{
+            Data0 = Data0 + Integer.toString(temp);
+        }
+         Randomizer = rand.nextInt(15);
+        temp = Start1 + Randomizer;
+        if (Length!=0)
+        {
+        Data1 = Data1 + Integer.toString(temp)+",";
+        }else{
+            Data1 = Data1 + Integer.toString(temp);
+        }       
+         Randomizer = rand.nextInt(20);
+        temp = Start2 + Randomizer;
+        if (Length!=0)
+        {
+        Data2 = Data2 + Integer.toString(temp)+",";
+        }else{
+            Data2 = Data2 + Integer.toString(temp);
+        }       
+        Randomizer = rand.nextInt(25);
+        temp = Start3 + Randomizer;
+        if (Length!=0)
+        {
+        Data3 = Data3 + Integer.toString(temp)+",";
+        }else{
+            Data3 = Data3 + Integer.toString(temp);
+        }      
+        Length=Length-1;
+        }
+        try{
+        PrintWriter writer = new PrintWriter(FileName);
+        writer.print("");
+        writer.close();
+        }        
+        catch(IOException e){
+            System.out.println("Error opening file");
+        }  
+        try{
+            FileWriter fout = new FileWriter(FileName,true);
+            BufferedWriter x = new BufferedWriter(fout);
+            PrintWriter pout = new PrintWriter(x);
+
+            pout.println(":"+StationRef+":");
+            pout.println(Data0);
+            pout.println(Data1);
+            pout.println(Data2);
+            pout.println(Data3);
+            pout.close();
+            x.close();
+            fout.close();
+        }
+        catch(IOException e){
+            System.out.println("Error opening file");
+        }    
+    }
 }
