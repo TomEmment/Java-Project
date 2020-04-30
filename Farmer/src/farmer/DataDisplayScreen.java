b9e8499190a1d8bbc3c6a0335d6dbd4f2a39f957
HarryEvs21 <56265991+HarryEvs21@users.noreply.github.com>
2020-04-30 18:22:57 +0100

    anohter one sorry

--- Farmer/src/farmer/DataDisplayScreen.java
+++ Farmer/src/farmer/DataDisplayScreen.java
@@ -4,12 +4,22 @@
  * and open the template in the editor.
  */
 package farmer;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+//import org.apache.poi.ss.usermodel.*;
 //import static javaserver.Server1.din;
 import java.awt.BorderLayout;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -27,6 +37,8 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import java.io.FileWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -307,6 +319,13 @@ public class DataDisplayScreen extends javax.swing.JFrame {
             
         }else{
             // Save file to Excel
+            String fileName = saveText.getText()+".xlsx";
+            try {
+                XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(new File(fileName)));
+            } catch (IOException ex) {
+                Logger.getLogger(DataDisplayScreen.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
         }
     }//GEN-LAST:event_saveButtonActionPerformed
 
